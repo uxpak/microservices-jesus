565cc9857bebf411b59fc04796367ff9a9176634
uxpak <117487835+uxpak@users.noreply.github.com>
2023-07-26 08:48:35 -0500

    Se agrega al proyecto

--- laptop-service/src/main/java/com/jesus/laptopservice/entity/Laptop.java
+++ laptop-service/src/main/java/com/jesus/laptopservice/entity/Laptop.java
@@ -21,5 +21,29 @@ public class Laptop {
     private String brand;
     private String model;
     private int userId;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
 
 }
